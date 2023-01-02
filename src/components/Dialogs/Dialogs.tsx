b51c735afec537b2ba917a6b2fbf89c5e53ddb75
Ni-speed <railmir@gmail.com>
2023-01-02 11:01:26 +0500

    lesson21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,9 +6,34 @@ type DiologPropsType = {
 }
 const Dialogs = (props: DiologPropsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+
+                    <div className={s.dialog + ' ' + s.active}>
+                        Ilmir
+                    </div>
+                    <div className={s.dialog}>
+                        Kamila
+                    </div>
+                    <div className={s.dialog}>
+                        Aliya
+                    </div>
+                    <div className={s.dialog}>
+                        Insiya
+                    </div>
+                    <div className={s.dialog}>
+                        Almaz
+                    </div>
+
+
+            </div>
+            <div className={s.masseges}>
+                <div className={s.masseg}>Hi!</div>
+                <div className={s.masseg}>How ara you?</div>
+                <div className={s.masseg}>I'am fine</div>
+            </div>
         </div>
     )
 }
-export default Dialogs
+
+    export default Dialogs;
